247f209da82a4cc78a2bbaf8bd873a9b6fd7ea21
MarshallThomp <50275240+MarshallThomp@users.noreply.github.com>
2019-07-19 16:42:43 -0600

    changes to seeds file

--- db/modules/create_module.sql
+++ db/modules/create_module.sql
@@ -1,2 +1,37 @@
-INSERT INTO modules2 (map_id, position1, position2)
-VALUES (${map_id}, ${position1}, ${position2});
+INSERT INTO modules2 (map_id, m2_position1, m2_position2)
+VALUES (${map_id}, ${m2_position1}, ${m2_position2});
+
+INSERT INTO modules3a (map_id, m3a_position1, m3a_position2, m3a_position3)
+VALUES (
+    ${map_id},
+    ${m3a_position1},
+    ${m3a_position2},
+    ${m3a_position3}
+);
+
+INSERT INTO modules3b (map_id, m3b_position1, m3b_position2, m3b_position3)
+VALUES (
+    ${map_id},
+    ${m3b_position1},
+    ${m3b_position2},
+    ${m3b_position3}
+);
+
+INSERT INTO modules4 (map_id, m4_position1, m4_position2, m4_position3, m4_position4)
+VALUES (
+    ${map_id}
+    ${m4_position1}
+    ${m4_position2}
+    ${m4_position3}
+    ${m4_position4}
+);
+
+INSERT INTO modules5 (map_id, m5_position1, m5_position2, m5_position3, m5_position4, m5_position5)
+VALUES (
+    ${map_id}
+    ${m5_position1}
+    ${m5_position2}
+    ${m5_position3}
+    ${m5_position4}
+    ${m5_position5}
+);
