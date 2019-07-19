b72c1474cba0df7d4d7963a814aee008fcaef3ce
rileyeh <rileyehatch@gmail.com>
2019-07-19 17:06:08 -0600

    login front end done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,26 +1,46 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './Home.scss'
 import {Link} from 'react-router-dom'
+import { connect } from 'react-redux'
+import {getUser} from '../../ducks/reducers/user'
 
-const Home = () => {
+const Home = props => {
+    useEffect(()=> {
+            props.getUser()
+    }, [props])
+
     return (
-        <body>
-            <section className='top-section'>
-                <h1 className='welcome'>welcome</h1>
-                <div className='video'></div>
-
-                <div className='buttons'>
-                    <Link to='/login'><button>login</button></Link>
-                    <Link to='/register'><button>register</button></Link>
+        <div>
+            {props.user
+                ?
+                <div>
+                    <h1 style={{'color':'white'}}>faction page</h1>f
                 </div>
-            </section>
+                :
+                <div className='body'>
+                <section className='top-section'>
+                    <h1 className='welcome'>welcome</h1>
+                    <div className='video'></div>
 
-            <section className='about-section'>
-                <h1>about</h1>
-                <p>info about the game and stuff</p>
-            </section>
-        </body>
+                    <div className='buttons'>
+                        <Link to='/login'><button>login</button></Link>
+                        <Link to='/register'><button>register</button></Link>
+                    </div>
+                </section>
+
+                <section className='about-section'>
+                    <h1>about</h1>
+                    <p>info about the game and stuff</p>
+                </section>
+            </div>}
+    </div>
     )
 }
 
-export default Home
+const mapStateToProps = state => {
+    return {
+        user: state.user.data
+    }
+}
+
+export default connect(mapStateToProps, {getUser})(Home)
