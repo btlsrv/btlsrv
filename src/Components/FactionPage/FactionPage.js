46f10a1ba71931fb4db7d6e14fabf58c4de792b4
rileyeh <rileyehatch@gmail.com>
2019-07-27 15:03:04 -0600

    forum functionality and styling done

--- src/Components/FactionPage/FactionPage.js
+++ src/Components/FactionPage/FactionPage.js
@@ -3,9 +3,13 @@ import './FactionPage.scss'
 import { connect } from 'react-redux'
 import { getUser } from '../../ducks/reducers/user'
 import axios from 'axios'
+import monkey from '../../Assets/cybermonkeys.svg'
+import alpaca from '../../Assets/alpacahackas.svg'
+import duck from '../../Assets/skylightducks.svg'
 
 const FactionPage = props => {
     const [faction, setFaction] = useState('')
+    const [picture, setPicture] = useState('')
     const [messages, setMessages] = useState([])
     const [message, setMessage] = useState('')
 
@@ -14,10 +18,14 @@ const FactionPage = props => {
         let {user} = props
         if (user.faction_id === 1) {
             setFaction('cyber monkeys')
+            setPicture(monkey)
         } else if (user.faction_id === 2) {
             setFaction('alpaca hackas')
+            setPicture(alpaca)
+
         } else {
             setFaction('skylight ducks')
+            setPicture(duck)
         }
         axios.get('api/messages').then(res => {
             let messagesArray = res.data
@@ -66,7 +74,10 @@ const FactionPage = props => {
 
     return (
         <div className='faction'>
-            <h1>{faction}</h1>
+            <div className='faction-header'>
+                <img src={picture} alt='faction picture id' style={{'width': 75, 'marginRight': 10}}/>
+                <h1>{faction}</h1>
+            </div>
 
             <section className='faction-main'>
                 <div className='leaderboard box-shadow'>
