f65553c4802303a5f2fbec767780be4ab8415e8c
MarshallThomp <50275240+MarshallThomp@users.noreply.github.com>
2019-07-29 17:51:02 -0600

    first draft of testing

--- cypress/integration/Register.spec.js
+++ cypress/integration/Register.spec.js
@@ -5,29 +5,29 @@ describe('testing register process', () => {
         cy.visit('/#/register')
     })
 
-    it('able to enter info in username input', () => {
+    it('able to enter info in username input', () => { // test done by Derek
         cy.get('input[name=username]')
             .type('taco')
             .should('have.value', 'taco')
     })
 
-    it('able to enter info in email input', () => {
+    it('able to enter info in email input', () => { // test done by Derek
         cy.get('input[name=email]')
         .type('taco@email.com')
         .should('have.value', 'taco@email.com')
     })
 
-    it('able to enter info in password input', () => {
+    it('able to enter info in password input', () => { // test done by Derek
         cy.get('input[name=password]')
         .type('password')
         .should('have.value', 'password')
     })
 
-    it('able to select a faction to join', () => {
+    it('able to select a faction to join', () => { // test done by Derek
         cy.get('select').select('cyber monkeys')
     })
 
-    it('able to register a new user', () => {
+    it('able to register a new user', () => { // test done by Derek
         cy.visit('/')
         cy.get('.buttons')
         .first()
