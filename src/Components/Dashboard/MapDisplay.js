d3eb524c39999d94f9c8e418e79b583963b337d9
Devin Black <Devinjamesblack@gmail.com>
2019-07-31 16:13:20 -0600

    display map added

--- src/Components/Dashboard/MapDisplay.js
+++ src/Components/Dashboard/MapDisplay.js
@@ -1,16 +1,26 @@
-import React, {  useState, useEffect } from 'react'
+import React, { Component } from 'react'
+import './MapDisplay.scss'
 import board from '../Gameboard/board'
+import Stoplight from '../Modules/Stoplight/Stoplight'
+import Threebar from '../Modules/ThreeBar/ThreeBar'
+import Sidebar from '../Modules/Sidebar/Sidebar'
+import TwoMiniDots from '../Modules/TwoMiniDots/TwoMiniDots'
+import ManyDots from '../Modules/ManyDots/ManyDots'
 import axios from 'axios'
 
 
-const MapDisplay = (props) => {
-  const [boardDisplay, setBoardDisplay] = useState([])
+class MapDisplay extends Component {
+  constructor(props) {
+    super(props)
 
-  let initialBoard = board.map( space => ({...space}))
+    this.state= {
+      board: []
+    }
+  }
 
-  
-  useEffect(() => {
-     axios.get(`/api/maps/${props.mapId}`).then(res => {
+  componentDidMount() {
+    let initialBoard = board.map( space => ({...space}))
+    axios.get(`/api/maps/${this.props.mapId}`).then(res => {
       const positions = res.data
       let mapBoard = initialBoard.map((space, i) => {
         for (let key in positions) {
@@ -20,31 +30,70 @@ const MapDisplay = (props) => {
         }
         return space
       })
-      setBoardDisplay(mapBoard)
+      let newBoard = mapBoard.map((space, i) => {
+      let { name } = space
+      if (name === 'm2_position1') {
+          space.comp = <Stoplight/>
+      } else if (name === 'm2_position2') {
+          space.comp = <Threebar/>
+      }  else if (name === 'm3a_position1') {
+          space.comp = <Sidebar/>
+      }  else if (name === 'm3a_position2') {
+          space.comp = <TwoMiniDots/>
+      }  else if (name === 'm3a_position3') {
+          space.comp = <Threebar/>
+      }  else if (name === 'm3b_position1') {
+          space.comp = <ManyDots/>
+      }  else if (name === 'm3b_position2') {
+          space.comp = <Stoplight/>
+      }  else if (name === 'm3b_position3') {
+          space.comp = <Sidebar/>
+      }  else if (name === 'm4_position1') {
+          space.comp = <ManyDots/>
+      }  else if (name === 'm4_position2') {
+          space.comp = <ManyDots/>
+      }  else if (name === 'm4_position3') {
+          space.comp = <ManyDots/>
+      }  else if (name === 'm4_position4') {
+          space.comp = <ManyDots/>
+      }  else if (name === 'm5_position1') {
+          space.comp = <TwoMiniDots/>
+      }  else if (name === 'm5_position2') {
+          space.comp = <Sidebar/>
+      }  else if (name === 'm5_position3') {
+          space.comp = <Threebar/>
+      }  else if (name === 'm5_position4') {
+          space.comp = <ManyDots/>
+      }  else if (name === 'm5_position5') {
+          space.comp = <Stoplight/>
+      } 
+      })
+      this.setState({
+        board: initialBoard
+      })
     })
-  }, [initialBoard, props])
-  console.log(props.mapId)
+  }
 
-  // const displayMapFalse = () => {
-  //   return (
-  //     props.displayMapShowFalse
-  //   )
-  // }
-  
-  return (
-    <div>
-    {boardDisplay.map((space, i) => {
-      return (
-        <div
-        key = {i}>
-          {space.comp}
-        </div>
-      )
-    })}
-    <div>
-      <button onClick={props.displayMapShowFalse}> X </button>
-    </div>
-    </div>
-  )
+  render() {
+    return (
+      <div className='map-display-main'>
+      <div className='map-display-board'>
+        {this.state.board.length > 0 && 
+          this.state.board.map((space, i) => {
+            console.log(777777, space)
+            return (
+              <div key={i}
+              className='board-space'>
+                {space.comp}
+              </div>
+            )
+          })
+        }
+        <button onClick={this.props.displayMapShowFalse}> X </button>
+      </div>
+      </div>
+    )
+  }
 }
+
 export default MapDisplay
--- src/Components/Gameboard/Gameboard.js
+++ src/Components/Gameboard/Gameboard.js
@@ -35,8 +35,8 @@ class Gameboard extends Component {
         this.state = {
             player1Map: initialBoard1,
             player2Map: initialBoard2,
-            player1Hits: 15,
-            player2Hits: 15,
+            player1Hits: 0,
+            player2Hits: 0,
             player2: false,
             currentTurn: 'player1',
             winner: '',
@@ -422,7 +422,6 @@ class Gameboard extends Component {
     }
 
     render() {
-        console.log(this.state)
         return (
             <>
             {this.props.player === 'player1' ? 
