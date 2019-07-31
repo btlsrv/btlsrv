c8fa3ad25bee5653eef1d2bd094c4d413e241a0b
rileyeh <rileyehatch@gmail.com>
2019-07-31 16:16:03 -0600

    added some styling

---
+++ src/Components/Home/Btlsrvboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import littlestoplight from '../../Assets/stoplight.svg'
+import littlethreebar from '../../Assets/threebar.svg'
+import littlesidebar from '../../Assets/sidebar.svg'
+import littletwominidots from '../../Assets/twominidots.svg'
+import littlemanydots from '../../Assets/manydots.svg'
+
+const Btlsrvboard = props => {
+    return (
+        <div></div>
+    )
+}
+
+export default Btlsrvboard
